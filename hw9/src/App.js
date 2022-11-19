ed4c634564aeb2f277bea502e722d07a298209f8
Ganesh <ganesh.asapu@icloud.com>
2022-11-19 05:08:53 -0500

    added database functionality

--- hw9/src/App.js
+++ hw9/src/App.js
@@ -1,21 +1,48 @@
-import {GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 //import { isButtonElement } from 'react-router-dom/dist/dom'
-import {auth} from './initialize'
+import { auth } from "./initialize";
+import React, { useRef } from "react";
+import { async } from "@firebase/util";
+import { firestore } from "./initialize";
+import { addDoc, collection } from "@firebase/firestore";
+
 function App() {
-  const googleProvider = new GoogleAuthProvider()
-  const GoogleLogin = async () => {
-    try{
-      const result = await signInWithPopup(auth, googleProvider)
-      console.log(result.user)
-    } catch(error){
-      console.log(error)
+  const googleProvider = new GoogleAuthProvider();
+  const messageRef = useRef();
+  const ref = collection(firestore, "rooms");
+
+  console.log(ref);
+
+  const addRoom = async (e) => {
+    e.preventDefault();
+    let data = {
+      message: messageRef.current.value,
+    };
+
+    try {
+      addDoc(ref, data);
+    } catch (e) {
+      console.log(e);
     }
-  }
+  };
+
+  const GoogleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      console.log(result.user);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-  <div>
-    <button onClick={GoogleLogin}></button> 
-    <div>Home</div>
-  </div>
+    <div>
+      <button onClick={GoogleLogin}>Auth</button>
+      <input type="text" ref={messageRef} />
+      <button type="submit" onClick={addRoom}>
+        Save
+      </button>
+      <div>Home</div>
+    </div>
   );
 }
 
