07ad689c87d1668e2648d4a8e9a595b5b89fbd87
Ganesh <ganesh.asapu@icloud.com>
2022-11-20 05:13:08 -0500

    database points

--- hw9/src/roomDisplay.js
+++ hw9/src/roomDisplay.js
@@ -11,6 +11,7 @@ export default function RoomDisplay({}) {
   const [allUsers, setAllUsers] = useState([]);
   const [roomInfo, setRoomInfo] = useState({});
   const [roomUsers, setRoomUsers] = useState([]);
+  const [pointData, setPointData] = useState([]);
   const date = moment();
   const [curDay, setCurDay] = useState(date);
 
@@ -25,7 +26,7 @@ export default function RoomDisplay({}) {
     navigate("/dashboard", { state: { test: state.test } });
   }
 
-  let roomID = state.roomId;
+  let roomID = "6f2iBYYI7xMJqE96YpDa";
 
   useEffect(() => {
     var rooms = [];
@@ -38,6 +39,7 @@ export default function RoomDisplay({}) {
 
       for (let allRoomID in rooms) {
         if (rooms[allRoomID].id == roomID) {
+          setPointData(rooms[allRoomID].scores);
           setRoomInfo(rooms[allRoomID]);
         }
       }
@@ -57,7 +59,6 @@ export default function RoomDisplay({}) {
   }, []);
 
   if (allUsers.length != 0 && roomInfo.length != 0 && roomUsers.length == 0) {
-    console.log(allUsers, roomInfo, roomUsers);
     var currentRoomUsers = [];
     for (let user in allUsers) {
       if (roomInfo.userIds.includes(allUsers[user].uid)) {
@@ -80,6 +81,9 @@ export default function RoomDisplay({}) {
 
   return (
     <div className="px-4 sm:px-6 lg:px-8">
+      {/* {console.log(pointData)}
+      {console.log(pointData[curDay.format("DD/MM/YY")])}
+      {console.log(Object.keys(pointData[curDay.format("DD/MM/YY")]))} */}
       <div style={{ height: "3vh" }} />
       <div className="sm:flex sm:items-center">
         <div className="sm:flex-auto">
@@ -117,7 +121,10 @@ export default function RoomDisplay({}) {
             >
               {"<"}
             </button>
-            <h1 className="text-4xl font-bold text-gray-900">
+            <h1
+              className="text-4xl font-bold text-gray-900"
+              style={{ marginLeft: "-5px", paddingRight: "15px" }}
+            >
               {curDay.format("MMM Do YYYY")}
             </h1>
             <div style={{ height: "5vh" }} />
@@ -145,6 +152,10 @@ export default function RoomDisplay({}) {
               <table className="min-w-full divide-y divide-gray-300">
                 <thead className="bg-gray-50">
                   <tr>
+                    <th
+                      scope="col"
+                      className="py-3.5 pl-1 pr-1 text-left text-xs font-light text-gray-900 sm:pl-6"
+                    ></th>
                     {roomInfo.activities ? (
                       roomInfo.activities.map((activity) => {
                         return (
@@ -163,43 +174,70 @@ export default function RoomDisplay({}) {
                   </tr>
                 </thead>
                 <tbody className="divide-y divide-gray-200 bg-white">
-                  {roomUsers.map((person) => (
-                    <tr key={person.id}>
-                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
-                        <div className="flex items-center">
-                          <div className="h-10 w-10 flex-shrink-0">
-                            <img
-                              className="h-10 w-10 rounded-full"
-                              src={person.image}
-                              alt=""
-                            />
-                          </div>
-                          <div className="ml-4">
-                            <div className="font-medium text-gray-900">
-                              {person.name}
+                  {roomUsers
+                    ? roomUsers.map((person, i) => (
+                        <tr key={i}>
+                          <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+                            <div className="flex items-center">
+                              <div className="h-10 w-10 flex-shrink-0">
+                                <img
+                                  className="h-10 w-10 rounded-full"
+                                  src={person.image}
+                                  alt=""
+                                />
+                              </div>
+                              <div className="ml-4">
+                                <div className="font-medium text-gray-900">
+                                  {person.name}
+                                </div>
+                                <div className="text-gray-500">
+                                  {person.email}
+                                </div>
+                              </div>
                             </div>
-                            <div className="text-gray-500">{person.email}</div>
-                          </div>
-                        </div>
-                      </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                        <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
-                          Active
-                        </span>
-                      </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                        {person.role}
-                      </td>
-                      <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                        <a
-                          href="#"
-                          className="text-indigo-600 hover:text-indigo-900"
-                        >
-                          Edit<span className="sr-only">, {person.name}</span>
-                        </a>
-                      </td>
-                    </tr>
-                  ))}
+                          </td>
+                          {Object.keys(
+                            pointData[curDay.format("DD/MM/YY")]
+                          ).map((activityName) => {
+                            //return <td>{activityName}</td>;
+                            for (let user in roomInfo.activities) {
+                              if (
+                                roomInfo.activities[user].name == activityName
+                              ) {
+                                for (let userId in pointData[
+                                  curDay.format("DD/MM/YY")
+                                ][activityName]) {
+                                  if (userId == person.uid) {
+                                    return (
+                                      <td
+                                        key={userId + activityName}
+                                        style={{ paddingLeft: "45px" }}
+                                      >
+                                        {
+                                          pointData[curDay.format("DD/MM/YY")][
+                                            activityName
+                                          ][userId]
+                                        }
+                                      </td>
+                                    );
+                                  }
+                                }
+                              }
+                            }
+                          })}
+
+                          <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                            <a
+                              href="#"
+                              className="text-indigo-600 hover:text-indigo-900"
+                            >
+                              Edit
+                              <span className="sr-only">, {person.name}</span>
+                            </a>
+                          </td>
+                        </tr>
+                      ))
+                    : ""}
                 </tbody>
               </table>
             </div>
